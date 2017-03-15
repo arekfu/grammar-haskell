0ccd073745f28b6a1165bedfa7e1ae15b92d0b4c
Davide Mancusi <davide.mancusi@cea.fr>
2017-03-15 18:29:59 +0100

    Have the system clock determine the seed for the random expansion

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Grammar
 import Grammar.Random
 
+import System.Clock
+
 exampleKeyValue :: [(Char, [String])]
 exampleKeyValue = let initialChars = map (:[]) ['a'..'c']
                       chars = ['a'..'c'] ++ ['0'..'3']
@@ -17,5 +19,6 @@ exampleGrammar = productionsToCharCFG 'E' exampleKeyValue
 main :: IO ()
 main = do print exampleGrammar
           let expansion = randomGrammarDeriveScan exampleGrammar
-          let strings = take 20 $ evalGrammar expansion 121333
+          TimeSpec _ seed <- getTime Realtime
+          let strings = take 20 $ evalGrammar expansion $ fromIntegral seed
           mapM_ putStrLn strings
