a867519c6af87d12ba1ecaef005e4cdf88ccfb0b
Davide Mancusi <davide.mancusi@cea.fr>
2017-03-16 18:32:53 +0100

    Improve tests for Grammar.Random

--- test/RandomTest.hs
+++ test/RandomTest.hs
@@ -13,25 +13,28 @@ import Grammar.Internal
 import Grammar.Random
 import GrammarTest (ACharCFG(..))
 
+-- | Generator of random sentences given an alphabet
 randomSentences :: [a] -> Gen [a]
 randomSentences syms = listOf1 $ elements syms
 
+-- | Terminals must not be expanded by the grammar rules
 prop_terminalsAreInvariant :: ACharCFG -> Seed -> Bool
 prop_terminalsAreInvariant (ACharCFG g) seed =
     let terms = getTerminals g
      in all (\sym -> evalGrammar (randomSymExpand g sym) seed == [sym]) terms
 
-prop_derive0IsId :: ACharCFG -> Seed -> Property
-prop_derive0IsId (ACharCFG g) seed =
+-- | Deriving a sentence for 0 steps must be the identity
+prop_deriveSent0IsId :: ACharCFG -> Seed -> Property
+prop_deriveSent0IsId (ACharCFG g) seed =
     let syms = toList $ getSymbols g
      in not (null syms) ==> forAll (randomSentences syms) $ \sent ->
             evalGrammar (randomSentDeriveN 0 g sent) seed === sent
 
-prop_deriveNYieldsNItems :: ACharCFG -> Seed -> Positive Int -> Property
-prop_deriveNYieldsNItems (ACharCFG g) seed (Positive n) =
+-- | Deriving the starting symbol for 0 steps must be the identity
+prop_deriveGrammar0IsId :: ACharCFG -> Seed -> Property
+prop_deriveGrammar0IsId (ACharCFG g) seed =
     let syms = toList $ getSymbols g
-     in not (null syms) ==> forAll (randomSentences syms) $ \sent ->
-            length (evalGrammar (randomSentDeriveN n g sent) seed) <= n
+     in not (null syms) ==> evalGrammar (randomGrammarDeriveN 0 g) seed === [startSymbol g]
 
 return []
 runTests :: IO Bool
