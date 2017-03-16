51f040267d329762169773f463f42961d808d2b0
Davide Mancusi <davide.mancusi@cea.fr>
2017-03-16 22:00:49 +0100

    Rename randomSentences -> sentences

--- test/RandomTest.hs
+++ test/RandomTest.hs
@@ -14,8 +14,8 @@ import Grammar.Random
 import GrammarTest (ACharCFG(..))
 
 -- | Generator of random sentences given an alphabet
-randomSentences :: [a] -> Gen [a]
-randomSentences syms = listOf1 $ elements syms
+sentences :: [a] -> Gen [a]
+sentences syms = listOf1 $ elements syms
 
 -- | Terminals must not be expanded by the grammar rules
 prop_terminalsAreInvariant :: ACharCFG -> Seed -> Bool
@@ -27,7 +27,7 @@ prop_terminalsAreInvariant (ACharCFG g) seed =
 prop_deriveSent0IsId :: ACharCFG -> Seed -> Property
 prop_deriveSent0IsId (ACharCFG g) seed =
     let syms = toList $ getSymbols g
-     in not (null syms) ==> forAll (randomSentences syms) $ \sent ->
+     in not (null syms) ==> forAll (sentences syms) $ \sent ->
             evalGrammar (randomSentDeriveN 0 g sent) seed === sent
 
 -- | Deriving the starting symbol for 0 steps must be the identity
