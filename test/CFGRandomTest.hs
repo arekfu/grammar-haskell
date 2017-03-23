acf24569276a3ceb0330aa9e78ecab027a0cb4af
Davide Mancusi <davide.mancusi@cea.fr>
2017-03-23 23:03:09 +0100

    Make Seed arguments Positive and Large in unit tests

--- test/CFGRandomTest.hs
+++ test/CFGRandomTest.hs
@@ -18,21 +18,21 @@ words :: [a] -> Gen [a]
 words syms = listOf1 $ elements syms
 
 -- | Terminals must not be expanded by the grammar rules
-prop_terminalsAreInvariant :: ACharCFG -> Seed -> Property
-prop_terminalsAreInvariant (ACharCFG g) seed =
+prop_terminalsAreInvariant :: ACharCFG -> Positive (Large Seed) -> Property
+prop_terminalsAreInvariant (ACharCFG g) (Positive (Large seed)) =
     let terms = getTerminals g
      in conjoin $ map (\sym -> evalMC (randomSymExpand g sym) seed == [sym]) $ toList terms
 
 -- | Deriving a word for 0 steps must be the identity
-prop_deriveWord0IsId :: ACharCFG -> Seed -> Property
-prop_deriveWord0IsId (ACharCFG g) seed =
+prop_deriveWord0IsId :: ACharCFG -> Positive (Large Seed) -> Property
+prop_deriveWord0IsId (ACharCFG g) (Positive (Large seed)) =
     let syms = toList $ getSymbols g
      in not (null syms) ==> forAll (words syms) $ \word ->
             evalMC (randomWordDeriveN 0 g word) seed === word
 
 -- | Deriving the starting symbol for 0 steps must be the identity
-prop_deriveGrammar0IsId :: ACharCFG -> Seed -> Property
-prop_deriveGrammar0IsId (ACharCFG g) seed =
+prop_deriveGrammar0IsId :: ACharCFG -> Positive (Large Seed) -> Property
+prop_deriveGrammar0IsId (ACharCFG g) (Positive (Large seed)) =
     let syms = toList $ getSymbols g
      in not (null syms) ==> evalMC (randomGrammarDeriveN 0 g) seed === [startSymbol g]
 
--- test/RegexRandomTest.hs
+++ test/RegexRandomTest.hs
@@ -5,6 +5,7 @@ module RegexRandomTest
 -- system imports
 import Test.QuickCheck
 import Data.Foldable (forM_)
+import Data.Coerce
 
 -- local imports
 import Grammar.MC
@@ -14,10 +15,10 @@ import RegexTest (ARegex(..))
 
 printExamples :: IO ()
 printExamples = do putStrLn "Performing random expansion of some random regexes..."
-                   seeds <- sample' (arbitrary :: Gen Seed)
+                   seeds <- sample' (arbitrary :: Gen (Positive (Large Seed)))
                    ars <- sample' (arbitrary :: Gen (ARegex NonTerminal))
                    forM_ ars $ \ar -> do
                      let r = unARegex ar
                      putStrLn $ "Expanding regex " ++ show r
-                     forM_ seeds $ \seed -> do
+                     forM_ ((coerce seeds)::[Seed]) $ \seed -> do
                          putStrLn $ concatMap show $ evalMC (randomExpandRegex r) seed
